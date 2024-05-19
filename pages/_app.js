a1b07612b8517f8c4fc9000b68914c2dbf0afadd
Brendan Erhard <erhardbr@gmail.com>
2024-05-19 10:51:53 -0400

    Integrate Navbar and Analytics components in _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -2,12 +2,14 @@
 import React from 'react';
 import '../styles/globals.css';  // Existing global styles
 import Navbar from '../components/Navbar';
+import { Analytics } from '@vercel/analytics/react';
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <Navbar />
       <Component {...pageProps} />
+      <Analytics />
     </>
   );
 }
